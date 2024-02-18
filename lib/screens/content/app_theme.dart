0689870e06b23ca4dea91f97ab1d1216501c1956
rytener <s.n.s04@mail.ru>
2024-02-18 12:03:29 +0300

    first

--- lib/screens/content/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
-
-final Color color1 = Color(0xff5c6f52);
-final Color color2 = Color(0xfff2f4ef);
-final Color color3 = Color(0xffffffff);
-
-
