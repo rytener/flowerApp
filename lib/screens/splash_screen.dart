cb4ac4738f6219e7c74f77be20149b3812df7edb
rytener <s.n.s04@mail.ru>
2024-02-19 21:44:11 +0300

    first

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      const Duration(seconds: 1),
+      const Duration(seconds: 2),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (_) => LoginPage(),
