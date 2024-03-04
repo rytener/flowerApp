72bdb076055f98f8ec29a9efb73f5116e7131bb5
rytener <s.n.s04@mail.ru>
2024-03-05 00:43:34 +0300

    first

--- lib/screens/components.dart
+++ lib/screens/components.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-abstract class AppColor{
+abstract class AppColor {
   static const Color mainLightGreen = Color(0xff57704F);
   static const Color SecondaryGreen = Color(0xff333F33);
   static const Color color3 = Color(0xffC7B8B1);
@@ -12,13 +12,28 @@ abstract class AppColor{
   static const Color color9 = Color(0xff1C1C1C);
   static const Color color10 = Color(0xffE84B51);
   static const Color color11 = Color(0xff5D5F61);
-  static const Color color12 = Color(0xffE5EAE0);
+  static const Color likeBackground = Color(0xffE5EAE0);
   static const Color color13 = Color(0xffFAD7D9);
   static const Color color14 = Color(0xff111A0F);
   static const Color color15 = Color(0xffFEFEFE);
   static const Color appBarShadow = Color(0xffD3D4CD);
 }
-abstract class MyTextStyles{
+
+abstract class MyTextStyles {
   static const standart = TextStyle(fontWeight: FontWeight.bold);
-  static const buttonTextStyle = TextStyle(fontWeight: FontWeight.bold,);
+  static const buttonTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+  );
+  static const latoRegularGreen600 = TextStyle(
+    fontFamily: 'Lato-Regular',
+    fontWeight: FontWeight.w600,
+    fontSize: 15.0,
+    color: AppColor.mainLightGreen,
+  );
+  static const latoRegularWhite600 = TextStyle(
+    fontFamily: 'Lato-Regular',
+    fontWeight: FontWeight.w600,
+    fontSize: 15.0,
+    color: Colors.white,
+  );
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:lilar_test_app/screens/components.dart';
-import 'autorization_screen.dart';
 import 'strings.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,79 +19,136 @@ class _HomeScreen extends State<HomeScreen> {
         backgroundColor: Colors.white,
         shadowColor: AppColor.appBarShadow,
         elevation: 0.2,
-        title: const Column(
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 AppBarContainers(
-                    sizeHeight: 48,
-                    sizeWidth: 286,
-                    radius: 16,
-                    containerColor: AppColor.color6),
+                  sizeHeight: 48,
+                  sizeWidth: 286,
+                  containerColor: AppColor.appBarShadow,
+                  child: const TextField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Введите название',
+                      icon: Padding(
+                        padding: EdgeInsets.only(left: 12.0, right: 8.0),
+                        child: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                ),
                 Padding(
-                  padding: EdgeInsets.only(left: 16),
+                  padding: const EdgeInsets.only(left: 16),
                   child: AppBarContainers(
                     sizeHeight: 48,
                     sizeWidth: 48,
                     radius: 12,
                     containerColor: AppColor.color3,
                     containerIcon: Icons.percent_outlined,
+                    child: const Image(image: AssetImage('assets/images/Vector.png'),),
                   ),
                 ),
               ],
             ),
             Padding(
-              padding: EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.only(top: 20),
               child: Row(
                 children: [
                   AppBarContainers(
-                      sizeHeight: 40,
-                      sizeWidth: 88,
-                      radius: 16,
-                      containerColor: AppColor.buttonShadow),
+                    sizeWidth: 88,
+                    child: const Center(
+                      child: Text(
+                        'Все',
+                        style: MyTextStyles.latoRegularGreen600,
+                      ),
+                    ),
+                  ),
                   Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
                     child: AppBarContainers(
-                        sizeHeight: 40,
-                        sizeWidth: 110,
-                        radius: 16,
-                        containerColor: AppColor.buttonShadow),
+                      containerColor: AppColor.mainLightGreen,
+                      sizeWidth: 110,
+                      child: const Center(
+                        child: Text(
+                          'Цветы',
+                          style: MyTextStyles.latoRegularWhite600,
+                        ),
+                      ),
+                    ),
                   ),
                   AppBarContainers(
-                      sizeHeight: 40,
-                      sizeWidth: 128,
-                      radius: 16,
-                      containerColor: AppColor.buttonShadow),
+                    sizeWidth: 128,
+                    child: const Center(
+                      child: Text(
+                        'Подарки',
+                        style: MyTextStyles.latoRegularGreen600,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
-      body: Container(
-        color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ProductCard(),
+                ProductCard(),
+              ],
+            ),
+            Row(
+              children: [
+                ProductCard(),
+                ProductCard(),
+              ],
+            ),
+            Row(
+              children: [
+                ProductCard(),
+                ProductCard(),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const NavigationBarIcons(
+        firstIcon: Icons.home,
+        firstLabel: 'Home',
+        secondIcon: Icons.home,
+        secondLabel: 'Home',
+        thirdIcon: Icons.home,
+        thirdLabel: 'Home',
+        fourthIcon: Icons.home,
+        fourthLabel: 'Home',
       ),
     );
   }
 }
-
-class AppBarContainers extends StatelessWidget {
-  final double sizeHeight;
-  final double sizeWidth;
-  final double radius;
-  final containerColor;
+ class AppBarContainers extends StatelessWidget {
+  double sizeHeight;
+  double sizeWidth;
+  double radius;
+  Color containerColor;
   final containerIcon;
   final String? text;
+  final child;
 
-  const AppBarContainers(
-      {required this.sizeHeight,
+  AppBarContainers(
+      {this.sizeHeight = 40,
       required this.sizeWidth,
-      required this.radius,
-      required this.containerColor,
+      this.radius = 16,
+      this.containerColor = AppColor.buttonShadow,
       this.containerIcon,
       this.text,
+      this.child,
       super.key});
 
   @override
@@ -105,7 +160,154 @@ class AppBarContainers extends StatelessWidget {
         color: containerColor,
         borderRadius: BorderRadius.circular(radius),
       ),
-      child: Icon(containerIcon),
+      child: child,
+    );
+  }
+}
+
+class ProductCard extends StatefulWidget {
+  double sizeHeight;
+  double sizeWidth;
+  double radius;
+  Color containerColor;
+  final String? productName;
+  final String? sellCost;
+  final String? discountCost;
+
+  ProductCard(
+      {this.sizeHeight = 200,
+      this.sizeWidth = 169,
+      this.radius = 16,
+      this.containerColor = AppColor.color6,
+      this.productName,
+      this.discountCost,
+      this.sellCost,
+      super.key});
+
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  bool like = false;
+  static const image = AssetImage('assets/images/Ic_Favorite.png');
+  AssetImage _containerImage =image ;
+
+  void _dislike() {
+    setState(() {
+      _containerImage = AssetImage('assets/images/Ic_Favorite.png');
+    });
+  }
+
+  void _like() {
+    setState(() {
+      _containerImage = AssetImage('assets/images/Ic_Favorite_filled.png');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 18, top: 20),
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Container(
+            height: widget.sizeHeight + 70,
+            width: widget.sizeWidth,
+            decoration: BoxDecoration(
+              color: widget.containerColor,
+              borderRadius: BorderRadius.circular(widget.radius),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(widget.radius),
+            child: Container(
+              height: widget.sizeHeight,
+              width: widget.sizeWidth,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(widget.radius),
+              ),
+              child: const Image(
+                image: AssetImage('assets/images/first.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {
+                if (like == false) {
+                  like = true;
+                  _like();
+                } else if (like == true) {
+                  like = false;
+                  _dislike();
+                }
+              },
+              child: Container(
+                height: 32,
+                width: 32,
+                decoration: BoxDecoration(
+                  color: AppColor.likeBackground,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Image(image: _containerImage),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationBarIcons extends StatelessWidget {
+  final IconData firstIcon;
+  final IconData secondIcon;
+  final IconData thirdIcon;
+  final IconData fourthIcon;
+  final String firstLabel;
+  final String secondLabel;
+  final String thirdLabel;
+  final String fourthLabel;
+
+  const NavigationBarIcons(
+      {required this.firstIcon,
+      required this.firstLabel,
+      required this.secondIcon,
+      required this.secondLabel,
+      required this.thirdIcon,
+      required this.thirdLabel,
+      required this.fourthIcon,
+      required this.fourthLabel,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: AppColor.greyText,
+      unselectedItemColor: AppColor.greyText,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(firstIcon),
+          label: firstLabel,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(firstIcon),
+          label: secondLabel,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(firstIcon),
+          label: thirdLabel,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(firstIcon),
+          label: fourthLabel,
+        ),
+      ],
     );
   }
 }
